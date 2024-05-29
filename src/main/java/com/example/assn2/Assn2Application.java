79769a0223217f8aa2e8a8f69fc84a19076abba8
onioin <canyongudaitis@gmail.com>
2024-05-29 13:01:01 -0700

    initial commit

---
+++ src/main/java/com/example/assn2/Assn2Application.java
@@ -0,0 +1,13 @@
+package com.example.assn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assn2Application.class, args);
+	}
+
+}
