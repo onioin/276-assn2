4d38a8ae5ee974d7b43c84dd34106d47b44fa779
onioin <canyongudaitis@gmail.com>
2024-06-12 11:03:29 -0700

    added add form

--- src/main/java/com/example/assn2/controllers/RectangleController.java
+++ src/main/java/com/example/assn2/controllers/RectangleController.java
@@ -2,13 +2,14 @@ package com.example.assn2.controllers;
 
 import com.example.assn2.models.Rectangle;
 import com.example.assn2.models.RectangleRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class RectangleController {
@@ -29,6 +30,17 @@ public class RectangleController {
         model.addAttribute("rect", rectRepo.findById(id));
 
         return "viewRectangle";
+    }
+
+    @PostMapping("/rectangles/add")
+    public String addRectangle(@RequestParam Map<String, String> new_rect, HttpServletResponse response) {
+        String new_name = new_rect.get("name");
+        String new_colour = new_rect.get("colour");
+        int new_width = Integer.parseInt(new_rect.get("width"));
+        int new_height = Integer.parseInt(new_rect.get("height"));
+        rectRepo.save(new Rectangle(new_name, new_colour, new_width, new_height));
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        return "redirect:/rectangles/view";
     }
 
 }
